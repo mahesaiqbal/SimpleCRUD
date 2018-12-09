28be2c82d91b38786d4e923dd95060203affe74a
iqbal <mahesa@popbox.asia>
2018-12-09 22:53:07 +0700

    Code cleanup

--- db/SelectData.php
+++ db/SelectData.php
@@ -5,43 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <style>
-        .footer {
-            position: fixed;
-            left: 0;
-            bottom: 0;
-            width: 100%;
-            height: 70px;
-            background-color: #111111;
-            color: white;
-            text-align: center;
-        }
-    </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-            <a class="navbar-brand" href="#">Mahesa Iqbal</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>    
-                </ul>
-            </div>  
-        </nav>
-        <br>
         <div class="container">
             <h2>Nilai Mahasiswa Universitas Budi Luhur</h2>
             <p>Data nilai mahasiswa</p>
@@ -105,9 +73,6 @@
             </table>
             <div>
                 <button onclick="location.href='/uas_pw2/form_nilai_mahasiswa.php'" class="btn btn-primary">Create a new data</button>
-            </div>
-            <div class="footer">
-                <p>Mahesa Iqbal Ridwansyah</p>
             </div>
         </div>
     </body>
